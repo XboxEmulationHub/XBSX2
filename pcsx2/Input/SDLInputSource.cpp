75684534db8abd5a3be2a10e11019ceef4e9e0bf
sirmangler <thecatsmangler@gmail.com>
2023-05-13 16:06:59 +0100

    Initial commit. Introduce WinRT/Xbox support.
    Warning text to let users know not to bother the PCSX2 devs with their problems

--- pcsx2/Input/SDLInputSource.cpp
+++ pcsx2/Input/SDLInputSource.cpp
@@ -1,3 +1,4 @@
+#if SDL_BUILD
 // SPDX-FileCopyrightText: 2002-2025 PCSX2 Dev Team
 // SPDX-License-Identifier: GPL-3.0+
 
@@ -1739,3 +1740,4 @@ bool SDLInputSource::IsControllerSixaxis(const ControllerData& cd)
 		   SDL_GetNumJoystickAxes(cd.joystick) == 16 &&
 		   SDL_GetNumJoystickButtons(cd.joystick) == 11;
 }
+#endif

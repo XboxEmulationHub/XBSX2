404e0c9040fc6d7c12b4a63dc672f328ad140dcf
SternXD <stern@sidestore.io>
2026-02-14 18:59:57 -0500

    UWP: Rename project from pcsx2-winrt to pcsx2-uwp

--- pcsx2/Pcsx2Config.cpp
+++ pcsx2/Pcsx2Config.cpp
@@ -43,7 +43,7 @@ static constexpr FPControlRegister DEFAULT_VU_FP_CONTROL_REGISTER = FPControlReg
 Pcsx2Config EmuConfig;
 
 #ifdef WINRT_XBOX
-#include "pcsx2-winrt/UWPUtils.h"
+#include "pcsx2-uwp/UWPUtils.h"
 #endif
 
 const char* SettingInfo::StringDefaultValue() const
